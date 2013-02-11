288aab8979aee3d033c8a25ba08e1f4b35fe311b
damien.honeyford@gmail.com <damien.honeyford@gmail.com@3ae0b7f8-ee8e-08e4-b4bb-1ea39429c5ee>
2013-02-11 16:38:44 +0000

    Added the ability to start at logon of current user/all users -- requires TaskScheduler library which is embedded in to the main assembly and linked at runtime.  Also added version check against the Changelog on the RealTemp4RTSS Google Code page.

--- trunk/RealTemp4RTSS.Designer.cs
+++ trunk/RealTemp4RTSS.Designer.cs
@@ -29,21 +29,23 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.ListViewGroup listViewGroup3 = new System.Windows.Forms.ListViewGroup("Temperatures", System.Windows.Forms.HorizontalAlignment.Left);
-            System.Windows.Forms.ListViewGroup listViewGroup4 = new System.Windows.Forms.ListViewGroup("Other", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("Temperatures", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("Other", System.Windows.Forms.HorizontalAlignment.Left);
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RealTemp4RTSS));
             this.lsvAvailableMetrics = new System.Windows.Forms.ListView();
             this.colMetric = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.colValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.notifyIconMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuShow = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
             this.realTempRefresh = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.chkStartMinimised = new System.Windows.Forms.CheckBox();
+            this.btnAdvanced = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnOK = new System.Windows.Forms.Button();
+            this.chkStartMinimised = new System.Windows.Forms.CheckBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.grpMetrics = new System.Windows.Forms.GroupBox();
             this.grpStatus = new System.Windows.Forms.GroupBox();
@@ -52,13 +54,21 @@
             this.lblRealTemp = new System.Windows.Forms.Label();
             this.lblOSD = new System.Windows.Forms.Label();
             this.lblRealTempStatus = new System.Windows.Forms.Label();
-            this.mnuShow = new System.Windows.Forms.ToolStripMenuItem();
+            this.grpAdvancedOptions = new System.Windows.Forms.GroupBox();
+            this.btnUpdateCheck = new System.Windows.Forms.Button();
+            this.chkNotifyUpdates = new System.Windows.Forms.CheckBox();
+            this.pnlStartupOptions = new System.Windows.Forms.Panel();
+            this.radStartForAllUsers = new System.Windows.Forms.RadioButton();
+            this.radStartForCurrentUser = new System.Windows.Forms.RadioButton();
+            this.chkStartWithWindows = new System.Windows.Forms.CheckBox();
             this.notifyIconMenu.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.grpMetrics.SuspendLayout();
             this.grpStatus.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
+            this.grpAdvancedOptions.SuspendLayout();
+            this.pnlStartupOptions.SuspendLayout();
             this.SuspendLayout();
             // 
             // lsvAvailableMetrics
@@ -71,13 +81,13 @@
             this.colMetric,
             this.colValue});
             this.lsvAvailableMetrics.FullRowSelect = true;
-            listViewGroup3.Header = "Temperatures";
-            listViewGroup3.Name = "lvgTemps";
-            listViewGroup4.Header = "Other";
-            listViewGroup4.Name = "lvgOther";
+            listViewGroup1.Header = "Temperatures";
+            listViewGroup1.Name = "lvgTemps";
+            listViewGroup2.Header = "Other";
+            listViewGroup2.Name = "lvgOther";
             this.lsvAvailableMetrics.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
-            listViewGroup3,
-            listViewGroup4});
+            listViewGroup1,
+            listViewGroup2});
             this.lsvAvailableMetrics.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.lsvAvailableMetrics.HideSelection = false;
             this.lsvAvailableMetrics.Location = new System.Drawing.Point(6, 22);
@@ -116,6 +126,13 @@
             this.notifyIconMenu.Name = "notifyIconMenu";
             this.notifyIconMenu.Size = new System.Drawing.Size(104, 48);
             // 
+            // mnuShow
+            // 
+            this.mnuShow.Name = "mnuShow";
+            this.mnuShow.Size = new System.Drawing.Size(103, 22);
+            this.mnuShow.Text = "Show";
+            this.mnuShow.Click += new System.EventHandler(this.notifyIcon_DoubleClick);
+            // 
             // mnuExit
             // 
             this.mnuExit.Name = "mnuExit";
@@ -141,7 +158,7 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.Control;
-            this.panel1.Controls.Add(this.chkStartMinimised);
+            this.panel1.Controls.Add(this.btnAdvanced);
             this.panel1.Controls.Add(this.btnCancel);
             this.panel1.Controls.Add(this.btnOK);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
@@ -150,25 +167,23 @@
             this.panel1.Size = new System.Drawing.Size(414, 46);
             this.panel1.TabIndex = 3;
             // 
-            // chkStartMinimised
+            // btnAdvanced
             // 
-            this.chkStartMinimised.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.chkStartMinimised.AutoSize = true;
-            this.chkStartMinimised.Location = new System.Drawing.Point(12, 15);
-            this.chkStartMinimised.Name = "chkStartMinimised";
-            this.chkStartMinimised.Size = new System.Drawing.Size(109, 19);
-            this.chkStartMinimised.TabIndex = 0;
-            this.chkStartMinimised.Text = "Start Minimised";
-            this.chkStartMinimised.UseVisualStyleBackColor = true;
-            this.chkStartMinimised.Click += new System.EventHandler(this.chkStartMinimised_CheckedChanged);
+            this.btnAdvanced.Location = new System.Drawing.Point(12, 10);
+            this.btnAdvanced.Name = "btnAdvanced";
+            this.btnAdvanced.Size = new System.Drawing.Size(75, 25);
+            this.btnAdvanced.TabIndex = 3;
+            this.btnAdvanced.Text = "Advanced";
+            this.btnAdvanced.UseVisualStyleBackColor = true;
+            this.btnAdvanced.Click += new System.EventHandler(this.btnAdvanced_Click);
             // 
             // btnCancel
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.Enabled = false;
-            this.btnCancel.Location = new System.Drawing.Point(327, 12);
+            this.btnCancel.Location = new System.Drawing.Point(327, 10);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(75, 25);
             this.btnCancel.TabIndex = 2;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -178,13 +193,27 @@
             // 
             this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnOK.Enabled = false;
-            this.btnOK.Location = new System.Drawing.Point(246, 12);
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnOK.Location = new System.Drawing.Point(246, 10);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.Size = new System.Drawing.Size(75, 25);
             this.btnOK.TabIndex = 1;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
             this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // chkStartMinimised
+            // 
+            this.chkStartMinimised.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkStartMinimised.AutoSize = true;
+            this.chkStartMinimised.Location = new System.Drawing.Point(14, 114);
+            this.chkStartMinimised.Name = "chkStartMinimised";
+            this.chkStartMinimised.Size = new System.Drawing.Size(109, 19);
+            this.chkStartMinimised.TabIndex = 2;
+            this.chkStartMinimised.Text = "Start minimised";
+            this.chkStartMinimised.UseVisualStyleBackColor = true;
+            this.chkStartMinimised.CheckedChanged += new System.EventHandler(this.chkStartMinimised_CheckedChanged);
+            this.chkStartMinimised.Click += new System.EventHandler(this.chkStartMinimised_CheckedChanged);
             // 
             // panel2
             // 
@@ -286,12 +315,85 @@
             this.lblRealTempStatus.TabIndex = 5;
             this.lblRealTempStatus.Text = "Polling...";
             // 
-            // mnuShow
+            // grpAdvancedOptions
             // 
-            this.mnuShow.Name = "mnuShow";
-            this.mnuShow.Size = new System.Drawing.Size(103, 22);
-            this.mnuShow.Text = "Show";
-            this.mnuShow.Click += new System.EventHandler(this.notifyIcon_DoubleClick);
+            this.grpAdvancedOptions.Controls.Add(this.btnUpdateCheck);
+            this.grpAdvancedOptions.Controls.Add(this.chkNotifyUpdates);
+            this.grpAdvancedOptions.Controls.Add(this.pnlStartupOptions);
+            this.grpAdvancedOptions.Controls.Add(this.chkStartWithWindows);
+            this.grpAdvancedOptions.Controls.Add(this.chkStartMinimised);
+            this.grpAdvancedOptions.Location = new System.Drawing.Point(8, 36);
+            this.grpAdvancedOptions.Name = "grpAdvancedOptions";
+            this.grpAdvancedOptions.Size = new System.Drawing.Size(399, 292);
+            this.grpAdvancedOptions.TabIndex = 4;
+            this.grpAdvancedOptions.TabStop = false;
+            this.grpAdvancedOptions.Text = "Advanced Options";
+            this.grpAdvancedOptions.Visible = false;
+            // 
+            // btnUpdateCheck
+            // 
+            this.btnUpdateCheck.Location = new System.Drawing.Point(280, 144);
+            this.btnUpdateCheck.Name = "btnUpdateCheck";
+            this.btnUpdateCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdateCheck.TabIndex = 4;
+            this.btnUpdateCheck.Text = "Check";
+            this.btnUpdateCheck.UseVisualStyleBackColor = true;
+            this.btnUpdateCheck.Click += new System.EventHandler(this.btnUpdateCheck_Click);
+            // 
+            // chkNotifyUpdates
+            // 
+            this.chkNotifyUpdates.AutoSize = true;
+            this.chkNotifyUpdates.Location = new System.Drawing.Point(14, 147);
+            this.chkNotifyUpdates.Name = "chkNotifyUpdates";
+            this.chkNotifyUpdates.Size = new System.Drawing.Size(237, 19);
+            this.chkNotifyUpdates.TabIndex = 3;
+            this.chkNotifyUpdates.Text = "Notify when there is an update available";
+            this.chkNotifyUpdates.UseVisualStyleBackColor = true;
+            this.chkNotifyUpdates.CheckedChanged += new System.EventHandler(this.chkNotifyUpdates_CheckedChanged);
+            // 
+            // pnlStartupOptions
+            // 
+            this.pnlStartupOptions.Controls.Add(this.radStartForAllUsers);
+            this.pnlStartupOptions.Controls.Add(this.radStartForCurrentUser);
+            this.pnlStartupOptions.Location = new System.Drawing.Point(27, 52);
+            this.pnlStartupOptions.Name = "pnlStartupOptions";
+            this.pnlStartupOptions.Size = new System.Drawing.Size(362, 50);
+            this.pnlStartupOptions.TabIndex = 1;
+            // 
+            // radStartForAllUsers
+            // 
+            this.radStartForAllUsers.AutoSize = true;
+            this.radStartForAllUsers.Location = new System.Drawing.Point(3, 28);
+            this.radStartForAllUsers.Name = "radStartForAllUsers";
+            this.radStartForAllUsers.Size = new System.Drawing.Size(72, 19);
+            this.radStartForAllUsers.TabIndex = 1;
+            this.radStartForAllUsers.TabStop = true;
+            this.radStartForAllUsers.Text = "Any User";
+            this.radStartForAllUsers.UseVisualStyleBackColor = true;
+            this.radStartForAllUsers.CheckedChanged += new System.EventHandler(this.radStartForAllUsers_CheckedChanged);
+            // 
+            // radStartForCurrentUser
+            // 
+            this.radStartForCurrentUser.AutoSize = true;
+            this.radStartForCurrentUser.Location = new System.Drawing.Point(3, 3);
+            this.radStartForCurrentUser.Name = "radStartForCurrentUser";
+            this.radStartForCurrentUser.Size = new System.Drawing.Size(99, 19);
+            this.radStartForCurrentUser.TabIndex = 0;
+            this.radStartForCurrentUser.TabStop = true;
+            this.radStartForCurrentUser.Text = "[Current User]";
+            this.radStartForCurrentUser.UseVisualStyleBackColor = true;
+            this.radStartForCurrentUser.CheckedChanged += new System.EventHandler(this.radStartForCurrentUser_CheckedChanged);
+            // 
+            // chkStartWithWindows
+            // 
+            this.chkStartWithWindows.AutoSize = true;
+            this.chkStartWithWindows.Location = new System.Drawing.Point(14, 30);
+            this.chkStartWithWindows.Name = "chkStartWithWindows";
+            this.chkStartWithWindows.Size = new System.Drawing.Size(225, 19);
+            this.chkStartWithWindows.TabIndex = 0;
+            this.chkStartWithWindows.Text = "Start when the following user logs on:";
+            this.chkStartWithWindows.UseVisualStyleBackColor = true;
+            this.chkStartWithWindows.CheckedChanged += new System.EventHandler(this.chkStartWithWindows_CheckedChanged);
             // 
             // RealTemp4RTSS
             // 
@@ -299,6 +401,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(414, 425);
+            this.Controls.Add(this.grpAdvancedOptions);
             this.Controls.Add(this.grpStatus);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
@@ -314,13 +417,16 @@
             this.Move += new System.EventHandler(this.RealTemp4RTSS_Move);
             this.notifyIconMenu.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.grpMetrics.ResumeLayout(false);
             this.grpStatus.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.grpAdvancedOptions.ResumeLayout(false);
+            this.grpAdvancedOptions.PerformLayout();
+            this.pnlStartupOptions.ResumeLayout(false);
+            this.pnlStartupOptions.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -348,6 +454,14 @@
         private System.Windows.Forms.Label lblOSD;
         private System.Windows.Forms.Label lblRealTempStatus;
         private System.Windows.Forms.ToolStripMenuItem mnuShow;
+        private System.Windows.Forms.Button btnAdvanced;
+        private System.Windows.Forms.GroupBox grpAdvancedOptions;
+        private System.Windows.Forms.Panel pnlStartupOptions;
+        private System.Windows.Forms.RadioButton radStartForAllUsers;
+        private System.Windows.Forms.RadioButton radStartForCurrentUser;
+        private System.Windows.Forms.CheckBox chkStartWithWindows;
+        private System.Windows.Forms.CheckBox chkNotifyUpdates;
+        private System.Windows.Forms.Button btnUpdateCheck;
     }
 }
 
